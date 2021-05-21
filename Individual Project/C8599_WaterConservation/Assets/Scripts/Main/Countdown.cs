62d4ef025ea9cc58ecca0691228ee0cc2cff3cec
GRaj-gitHub <rajgopika1105@gmail.com>
2021-05-21 21:54:41 +0100

    Initial commit

---
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/Main/Countdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+    private int timeRemaining = 25;
+    [SerializeField]
+    private GameObject timeField;
+
+    void Start()
+    {
+        timeField.GetComponent<Text>().text = timeRemaining + "";
+        StartCoroutine(CountdownTimer());
+
+    }
+
+    IEnumerator CountdownTimer()
+    {
+        yield return new WaitForSeconds(1.0f);
+        timeRemaining--;
+        timeField.GetComponent<Text>().text = timeRemaining + "";
+        if(timeRemaining == 0)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            StartCoroutine(CountdownTimer());
+        }
+       
+    }
+}
