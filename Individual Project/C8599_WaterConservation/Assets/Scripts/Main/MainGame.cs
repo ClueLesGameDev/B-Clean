9b79ca41e0a7a93bf1a5fdfb7315545b683b5774
GRaj-gitHub <rajgopika1105@gmail.com>
2021-06-18 15:56:03 +0100

    added new assets

---
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/Main/MainGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    public Camera playerCam;
+    //public GameObject popup;
+
+    int platecount = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+            
+            bool isHit = Physics.Raycast(playerCam.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (isHit)
+            {
+                {
+                    if(hitInfo.transform.gameObject.tag == "MovableObj")
+                    {
+                        CollectPlates();
+                        Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                        //popup.SetActive(true);
+                    }
+                }
+               
+            }
+
+        }
+    }
+
+    public void CollectPlates()
+    {
+       if (platecount > 3)
+        {
+            Debug.Log("limit");
+            return;
+        }
+        platecount++;
+        Debug.Log(platecount);
+    }
+         
+}
