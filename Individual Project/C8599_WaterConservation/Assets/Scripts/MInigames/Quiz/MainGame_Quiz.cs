3e2945ec3d6eb4777be5ee58e2d8513207d5ec38
GRaj-gitHub <rajgopika1105@gmail.com>
2021-08-10 18:40:00 +0100

    kuch

---
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/MInigames/Quiz/MainGame_Quiz.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGame_Quiz : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject QuestionBox;
+
+    public TextAsset QuestionsText;
+    public TextAsset AnswerText;
+
+    private Dictionary<int, string> QuestionBank = new Dictionary<int, string>();
+    
+    private Dictionary<int, string> OptionSet = new Dictionary<int, string>();
+    
+    private Dictionary<int, string> AnswerSet = new Dictionary<int, string>();
+
+   
+    
+    private void Start()
+    {
+        LoadQuestions();
+
+       // LoadOptions();
+
+        LoadAnswers();
+
+        QuestionSetGenerator();
+    }
+
+    void LoadQuestions()
+    {
+        string Qtemp = null;
+        string Qlines = QuestionsText.text;
+        int count = 0;
+        for (int i = 0; i < Qlines.Length; i++)
+        {
+            Qtemp += Qlines[i];
+            if (Qlines[i] == '\n')
+            {
+                QuestionBank.Add(count, Qtemp);
+                count++;
+                Qtemp = null;
+
+            }
+        }
+    }
+
+
+    //void LoadOptions()
+    //{
+
+    //}
+
+
+    void LoadAnswers()
+    {
+        string Atemp = null;
+        string Alines = AnswerText.text;
+        int count = 0;
+        for (int i = 0; i < Alines.Length; i++)
+        {
+            Atemp += Alines[i];
+            if (Alines[i] == '\n')
+            {
+                AnswerSet.Add(count, Atemp);
+                count++;
+                Atemp = null;
+
+            }
+        }
+    }
+
+    void QuestionSetGenerator()
+    {
+        int randomNo = Random.Range(0, QuestionBank.Count);
+        string currentQuest = QuestionBank[randomNo];
+
+        QuestionBox.GetComponent<Text>().text = currentQuest;
+    }
+
+   
+}
---
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/Main/DirtyDishes/loadLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+    public void loadDirtyDishes()
+    {
+        SceneManager.LoadScene("DirtyDishes", LoadSceneMode.Single);
+    }
+
+    public void loadCatchThatDrop()
+    {
+        SceneManager.LoadScene("CatchThatDrop", LoadSceneMode.Single);
+    }
+
+    public void loadSpotThatGerm()
+    {
+        SceneManager.LoadScene("SpotThatGerm", LoadSceneMode.Single);
+    }
+}
