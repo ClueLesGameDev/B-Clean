7cfcad8e84d7bddebaf11f531cba605e66148cdf
GRaj-gitHub <rajgopika1105@gmail.com>
2021-09-04 23:21:24 +0100

    carwash_camera

---
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/Main/CarWash/CameraMovement_CW.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement_CW : MonoBehaviour
+{
+    public float speed = 3.5f;
+    private float X;
+    private float Y;
+
+    float scrollSpeed = 2000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 tempPos = transform.position;
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * speed, -Input.GetAxis("Mouse X") * speed, 0));
+            X = transform.rotation.eulerAngles.x;
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(X, Y, 0);
+        }
+
+        tempPos.z += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * 10 * Time.deltaTime;
+
+        tempPos.z = Mathf.Clamp(tempPos.z,-580,-125);
+
+        transform.position = tempPos;
+        
+      
+    }
+}
