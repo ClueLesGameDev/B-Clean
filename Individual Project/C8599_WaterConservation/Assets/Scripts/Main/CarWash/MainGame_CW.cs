12808d18be6cf3dff7721ca89f447f7b240adfcd
GRaj-gitHub <rajgopika1105@gmail.com>
2021-09-05 23:04:56 +0100

    aa

--- Individual Project/C8599_WaterConservation/Assets/Scripts/Main/CarWash/MainGame_CW.cs
+++ Individual Project/C8599_WaterConservation/Assets/Scripts/Main/CarWash/MainGame_CW.cs
@@ -19,20 +19,22 @@ public class MainGame_CW : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
 
-            RaycastHit hitInfo = new RaycastHit();
+            RaycastHit2D hitInfo = new RaycastHit2D();
 
-            bool isHit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            bool isHit = Physics2D.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
             if (isHit)
             {
                 {
-                    if (hitInfo.transform.gameObject.tag == "Collectible")
-                    {
+                    //if (hitInfo.transform.gameObject.tag == "Collectible")
+                    //{
                        // CollectPlates(hitInfo.transform.gameObject);
                         Debug.Log("Hit " + hitInfo.transform.gameObject.name);
-                    }
+                    //}
                 }
 
             }
+
+
 
         }
 
